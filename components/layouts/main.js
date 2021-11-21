27406b8ece4c6ae55494da6e5c93ea10803f00c1
crendo11 <crendo11@eafit.edu.co>
2021-11-21 15:15:56 -0500

    main padding correction

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -4,7 +4,7 @@ import { Box, Container } from '@chakra-ui/react';
 
 const Main = ({ children, router }) => {
     return (
-        <Box as="maain" pb={89}>
+        <Box as="main" pb={8}>
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <title> Cristian Rendon - Homepage </title>
