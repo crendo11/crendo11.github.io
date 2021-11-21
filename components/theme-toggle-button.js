54dd37b647a34d17ee3cf86415497b5bcfa2b25f
crendo11 <crendo11@eafit.edu.co>
2021-11-21 02:29:48 -0500

    solved issue in icon animation

--- components/theme-toggle-button.js
+++ components/theme-toggle-button.js
@@ -6,18 +6,21 @@ const ThemeToggleButton = () => {
     const { toggleColorMode } = useColorMode();
 
     return (
-        <AnimatePresence exitBeforeenter initial={false}>
-            <motion.div syle={{ display: 'inline-block' }}
+        <AnimatePresence exitBeforeEnter initial={false}>
+            <motion.div
+                style={{ display: 'inline-block' }}
                 key={useColorModeValue('light', 'dark')}
                 initial={{ y: -20, opacity: 0 }}
                 animate={{ y: 0, opacity: 1 }}
                 exit={{ y: 20, opacity: 0 }}
                 transition={{ duration: 0.2 }}
             >
-                <IconButton aria-label="Toggle theme"
+                <IconButton
+                    aria-label="Toggle theme"
                     colorScheme={useColorModeValue('purple', 'orange')}
                     icon={useColorModeValue(<MoonIcon />, <SunIcon />)}
-                    onClick={toggleColorMode}></IconButton>
+                    onClick={toggleColorMode}
+                ></IconButton>
             </motion.div>
         </AnimatePresence>
     )
