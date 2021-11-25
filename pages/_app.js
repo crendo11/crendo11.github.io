005a613fc994cdb980f83ef1153435f87103eb95
crendo11 <crendo11@eafit.edu.co>
2021-11-25 00:41:48 -0500

    unsaved changes in app

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,16 @@ import { ChakraProvider } from "@chakra-ui/react";
 import Layoout from '../components/layouts/main';
 import Fonts from '../components/fonts';
 import theme from '../lib/theme';
+import { AnimatePresence } from 'framer-motion';
 
-const Website = ( { Component, pageProps, router } ) => {
+const Website = ({ Component, pageProps, router }) => {
     return (
         <ChakraProvider theme={theme}>
             <Fonts />
             <Layoout router={router}>
-                <Component {...pageProps} key={ router.router } />
+                <AnimatePresence exitBeforeEnter initial={true}>
+                    <Component {...pageProps} key={router.router} />
+                </AnimatePresence>
             </Layoout>
         </ChakraProvider>
     );
